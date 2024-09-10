121b61db7153006ef054669186f318d729e0ebc8
Agesh k s <71646123+Ageshks@users.noreply.github.com>
2024-09-10 14:44:17 +0530

    Add files via upload

---
+++ hello/bin/hello.dart
@@ -0,0 +1,63 @@
+import 'package:hello/hello.dart' as hello;
+
+// void main(List<String> arguments) {
+//   print('Hello world!!');
+//   print("poda potta");
+// }
+
+
+// // arrow function that calculate interest
+// double calculateInterest(double principal, double rate, double time) =>
+//     principal * rate * time / 100;
+
+// void main() {
+//   double principal = 5000;
+//   double time = 3;
+//   double rate = 3;
+
+//   double tester = calculateInterest(principal, rate, time);
+//   print("The simple interest is $tester.");
+// }
+
+
+// function that calculate interest without aroow function
+// double calculateInterest(double principal, double rate, double time) {
+//   double interest = principal * rate * time / 100;
+//   return interest;
+// }
+
+// void main() {
+//   double principal = 5000;
+//   double time = 3;
+//   double rate = 3;
+
+//   double interest = calculateInterest(principal, rate, time);
+//   print("The simple interest is $interest.");
+// }
+
+
+// class Interval{
+//   void numbers (){
+//     for(int i=0;i<=10;i++){
+//       print(i);
+//     }
+//   }
+  
+// }
+
+// void main(){
+// Interval interval = Interval();
+//   interval.numbers();
+// }
+
+// void main(){
+//   List<String> footballplayers=['Ronaldo','Messi','Neymar','Hazard'];
+//   footballplayers.forEach( (names)=>print(names));
+// }
+
+void main() {
+  for (int i = 0; i < 5; i++) {
+    print("Iteration $i");
+  }
+}
+
